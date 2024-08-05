3636ea0767284d063efe1730c2f2b2f093af198b
narupanarts@gmail.com <narupanarts@gmail.com>
2024-08-05 22:03:52 +0700

    update

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,47 @@
-from django.db import models
+"""
+Database model
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
+class UserManager(BaseUserManager):
+    """Manager for user"""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create save and return new user"""
+        if not email:
+            raise ValueError('You must have an email address.')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    # adminarm1234
+#  docker-compose run --rm app sh -c "python manage.py createsuperuser"
+    def create_superuser(self, email, password):
+        """Create and return a new superuser."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    # Loging with django admin 
+    is_staff = models.BooleanField(default=False)
+    
+    objects = UserManager()
+    
+    USERNAME_FIELD = 'email'
